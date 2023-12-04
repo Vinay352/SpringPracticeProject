396eeeddc37292a9d72130a048d9594cfae58748
Vinay352 <vinay.j3097@gmail.com>
2023-12-04 04:17:04 -0500

    topic controller
    topic controller

--- demo1/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ demo1/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -2,17 +2,18 @@ package io.javabrains.springbootstarter.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("Spring", "Spring Framework", "Spring Framework Description"),
             new Topic("React", "React Framework", "React Framework Description"),
             new Topic("Java", "Java Framework", "Java Framework Description")
-    );
+    ));
 
     public List<Topic> getAllTopics(){
         return topics;
@@ -22,8 +23,21 @@ public class TopicService {
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
     }
 
-
     public void addTopic(Topic topic) {
         topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for(int i = 0; i < topics.size(); i++){
+            Topic t = topics.get(i);
+            if(t.getId().equals(id)){
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
     }
 }
